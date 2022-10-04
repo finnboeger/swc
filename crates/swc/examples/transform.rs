7b9ffcb5072702a461fa2d03a5d653b727a5b757
Finn Böger <finnboeger@googlemail.com>
2022-10-04 16:34:06 +0200

    fix missing globals (due to #5975)

--- crates/swc/examples/transform.rs
+++ crates/swc/examples/transform.rs
@@ -2,7 +2,7 @@ use std::{path::Path, sync::Arc};
 
 use anyhow::Context;
 use swc::{self, config::Options, try_with_handler, HandlerOpts};
-use swc_common::SourceMap;
+use swc_common::{Globals, SourceMap, GLOBALS};
 
 fn main() {
     let cm = Arc::<SourceMap>::default();
@@ -19,14 +19,17 @@ fn main() {
                 .load_file(Path::new("examples/transform-input.js"))
                 .expect("failed to load file");
 
-            c.process_js_file(
-                fm,
-                handler,
-                &Options {
-                    ..Default::default()
-                },
-            )
-            .context("failed to process file")
+            let globals = Globals::new();
+            GLOBALS.set(&globals, || {
+                c.process_js_file(
+                    fm,
+                    handler,
+                    &Options {
+                        ..Default::default()
+                    },
+                )
+                .context("failed to process file")
+            })
         },
     )
     .unwrap();
